060640d519cd429f0e1f1f5fd487a46a5210f1fc
Filip Dziedzic <dziedzic.filip@gmail.com>
2013-11-02 17:12:09 +0100

    Add ajax calls, add some input validation (especially escaping html/xml), add error allert when sending emot without username specified. Ready for test deployment

--- app/controllers/ChatApplication.scala
+++ app/controllers/ChatApplication.scala
@@ -2,17 +2,22 @@ package controllers
 
 import play.api.mvc.{Action, Controller}
 import play.api.libs.EventSource
-import play.api.libs.json.JsValue
+import play.api.libs.json._
 import play.api.libs.iteratee.{Concurrent, Enumeratee}
 
 /**
- *
+ * Chat controller for serving messages.
+ * Handle both incoming and outgoing messages.
  *
  * @author fdziedzic
  *         Date 29.10.13
  *         Time 16:35
  */
 object ChatApplication extends Controller {
+
+  case class Msg(room: String, user: String, msg: String, time: String)
+
+  implicit val msgFormat = Json.format[Msg]
 
   /** Central hub for distributing chat messages */
   val (chatOut, chatChannel) = Concurrent.broadcast[JsValue]
@@ -23,8 +28,25 @@ object ChatApplication extends Controller {
   }
 
   /** Controller action for POSTing chat messages */
-  def postMessage = Action(parse.json) {
-    req => chatChannel.push(req.body); Ok
+  def postMessage = Action(parse json) {
+    req =>
+      req.body.validate[Msg].map {
+        case msg => {
+          if (msg.user.trim.size != 0) {
+            val escaped = Msg(msg.room,
+              xml.Utility.escape(msg.user),
+              xml.Utility.escape(msg.msg),
+              xml.Utility.escape(msg.time)
+            )
+            chatChannel push Json.toJson(escaped)
+            Ok(Json.obj("status" -> "ok"))
+          } else {
+            BadRequest(Json.obj("status" -> "error: user cannot be empty"))
+          }
+        }
+      }.recoverTotal {
+        e => BadRequest(Json.obj("status" -> "error"))
+      }
   }
 
   /** Enumeratee for filtering messages based on room */
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,29 +1,24 @@
 $(function () {
+
+    /** set message time and send it by invoking sendMsg function */
     $('#sendForm').submit(function (event) {
-		console.log();
-		msg = $('#msg').val();
-		time = new Date();
-		sendMsg(msg, time)
-		$('#chat').append(enchanceMsg(msg, time));
-		$('#msg').val('');
-		$("#chat").scrollTop($("#chat")[0].scrollHeight);
+        msg = $('#msg').val();
+        time = new Date();
+        sendMsg(msg, time)
         event.preventDefault();
     });
 
-    $("#chat").html(function () {
-        return replaceEmoticons($(this).html());
+    /** listen the chat div for change its html and replace emoticons with their images if so */
+    $('#chat').bind("DOMSubtreeModified", function () {
+        var html = $('#chat').html();
+        for (e in emoticons) {
+            if (html.indexOf(e) !== -1) {
+                $('#chat').html(replaceEmoticons(html));
+            }
+        }
     });
-	
-	$('#chat').bind("DOMSubtreeModified",function(){
-		console.log('chat change');
-		var html = $('#chat').html();
-		for (e in emoticons) {
-			if (html.indexOf(e) !== -1) {
-				$('#chat').html(replaceEmoticons(html));
-			}
-		}
-	});
 
+    /** add emoticon string representation to msg input text */
     $(".emot").click(function () {
         icon = $(this).attr("value");
         $('#msg').val(function (index, val) {
@@ -31,83 +26,130 @@ $(function () {
         });
     });
 
+    /** listen the roomSelect for change, update global chat num and invoke listening */
     $("#roomSelect").change(function () {
-		console.log($(this).val());
-        if ($(this).val() === 'room1') {
-            alert("asdsa");
+        if (chatRoom !== $(this).val()) {
+            chatRoom = $(this).val();
+            $('#chat').html('');
+            listen(chatRoom);
         }
     });
 
+    /** add drag and drop event to all emoticonst */
     var emots = document.querySelectorAll('.emot');
     [].forEach.call(emots, function (emot) {
         emot.addEventListener('dragstart', emotStart, false);
     });
 
+    /** add drag and drop events to chat div */
     var chat = document.querySelector('.chat');
     chat.addEventListener('dragover', chatOver, false);
     chat.addEventListener('drop', chatDrop, false);
 
+    /** handle incoming messages: add to chat div.
+     * Prepend is used in order to keep the latest messages on top,
+     * otherwise auto scroll to the bottom of div is mandatory
+     * however, it does not work... */
+    function addMsg(msg) {
+        data = JSON.parse(msg.data);
+        $('#chat').prepend(enchanceMsg(data.msg, data.time, data.user));
+        $('#msg').val('');
+//        $('#chat').scrollTop($('#chat')[0].scrollHeight);
+    };
+
+    /** start listening on messages from selected room */
+    function listen(room) {
+        console.log(room);
+        chatFeed = new EventSource("/chatFeed/" + room);
+        chatFeed.addEventListener("message", addMsg, false);
+    };
+
 });
+
+// Global chat room
+
+var chatRoom;
 
 // Text manipulation
 
-function enchanceMsg(msg, time) {
-    // TODO format date
-	return 'At: ' + time.toUTCString() + '<br />' +
-		'<strong>' + $('#user').val()
-		+ ': </strong>' + msg + '<hr />';
+function enchanceMsg(msg, time, user) {
+    return '<strong>At</strong>: ' + time + '<br />' +
+        '<strong>' + user + ': </strong>' + msg + '<hr />';
 }
 
 // Ajax calls
 
 function sendMsg(msg, time) {
-	json = {
-		'room' : $('#roomSelect').val(),
-		'user' : $('#user'),
-		'msg' : msg,
-		'time' : time
-	}
-    // TODO ajax call
+    json = {
+        room: $('#roomSelect').val(),
+        user: $('#user').val(),
+        msg: msg,
+        time: dtFormat(time)
+    }
+    console.log(json);
+    $.ajax({
+        url: '/chat',
+        type: "POST",
+        data: JSON.stringify(json),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data) {
+            console.log('success');
+        }
+    })
 }
 
 // Drag and Drop event functions
 
 function emotStart(e) {
-    console.log("dragstart");
     e.dataTransfer.effectAllowed = 'copy';
     e.dataTransfer.setData('emot', this.id);
 };
 
-function chatDrop(e) {
-    // TODO call submit on sendForm
-	el = e.dataTransfer.getData('emot');
-	time = new Date();
-	msg = $('#' + el).attr('value');
-	sendMsg(msg, time);
-    console.log('chatDrop');
-    $("#chat").append(enchanceMsg(msg, time));
+function chatOver(e) {
+    if (e.preventDefault) e.preventDefault();
+    e.dataTransfer.dropEffect = 'copy';
 }
 
-function chatOver(e) {
-    e.preventDefault();
-    e.dataTransfer.dropEffect = 'copy';
+function chatDrop(e) {
+    if (e.stopPropagation) e.stopPropagation();
+    if ($('#user').val().trim() !== '') {
+        el = e.dataTransfer.getData('emot');
+        time = new Date();
+        msg = $('#' + el).attr('value');
+        sendMsg(msg, time);
+    } else {
+        if ($('#error-ph').html() === '') {
+            $('#error-ph').append('<div id="error" class="alert alert-error"><a class="close" data-dismiss="alert">&times;</a><strong>Error!</strong> Please specify your name.</div>');
+            $('#error').delay(3500).fadeOut("slow", function () {
+                $(this).remove();
+            });
+        }
+    }
+    if (e.preventDefault) {
+        e.preventDefault();
+    }
+    if (e.stopPropagation) {
+        e.stopPropagation();
+    }
+    return false;
 }
 
 // Emoticons tranformations
 
 var emoticons = {
-		':D': 'emot03.png',
-		':-)': 'emot09.png',
-		':o': 'emot11.png',
-		':&lt;3': 'emot14.png',
-		':)': 'emot15.png',
-		':-?': 'emot17.png'
-	};
+    ':D': 'emot03.png',
+    ':-)': 'emot09.png',
+    ':o': 'emot11.png',
+    ':&lt;3': 'emot14.png',
+    ':)': 'emot15.png',
+    ':-?': 'emot17.png'
+};
 
 function replaceEmoticons(text) {
-	var url = "./assets/images/";
-	var patterns = [];
-	var metachars = /[[\]{}()*+?.\\|^$\-,&#\s]/g;
+    var url = "./assets/images/";
+    var patterns = [];
+    var metachars = /[[\]{}()*+?.\\|^$\-,&#\s]/g;
 
     // build a regex pattern for each defined property
     for (var i in emoticons) {
@@ -122,4 +164,31 @@ function replaceEmoticons(text) {
             '<img src="' + url + emoticons[match] + '" width="40" height="40" />' :
             match;
     });
+}
+
+// Date formatter...
+
+function dtFormat(now) {
+    year = "" + now.getFullYear();
+    month = "" + (now.getMonth() + 1);
+    if (month.length == 1) {
+        month = "0" + month;
+    }
+    day = "" + now.getDate();
+    if (day.length == 1) {
+        day = "0" + day;
+    }
+    hour = "" + now.getHours();
+    if (hour.length == 1) {
+        hour = "0" + hour;
+    }
+    minute = "" + now.getMinutes();
+    if (minute.length == 1) {
+        minute = "0" + minute;
+    }
+    second = "" + now.getSeconds();
+    if (second.length == 1) {
+        second = "0" + second;
+    }
+    return year + "-" + month + "-" + day + " " + hour + ":" + minute + ":" + second;
 }
