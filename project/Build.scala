3ca163c6eceb75c80e337b8e3d0a363728b275cc
Filip Dziedzic <filipdz@student.agh.edu.pl>
2013-12-09 19:59:11 +0100

    Change version to 2.0.RC2

--- project/Build.scala
+++ project/Build.scala
@@ -5,7 +5,7 @@ import play.Project._
 object ApplicationBuild extends Build {
 
   val appName         = "cs-awp"
-  val appVersion      = "1.0-SNAPSHOT"
+  val appVersion      = "2.0.RC2"
 
   val appDependencies = Seq(
     // Add your project dependencies here,
